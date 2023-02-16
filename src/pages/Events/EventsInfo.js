4213958c4cedad12eb34a355c85df305d8067f2a
Jazem Siddiqui <jsiddiqui@Jazems-MacBook-Air.local>
2023-02-15 18:03:18 -0800

    update photos

--- src/pages/Events/EventsInfo.js
+++ src/pages/Events/EventsInfo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from "../../styles/Events/EventsInfo.module.css"
+import CultureShow from "../../assets/events/culture_show.png"
+import MKR from "../../assets/events/mkrplaceholder.jpeg"
+import Seminar from "../../assets/events/pakistaniseminar.jpeg"
 
 function EventsInfo() {
     return ( 
         <div className={styles.container}>
         <div className={styles.gridItem}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" alt="" />
+            <img src={CultureShow} alt="" />
         </div>
        <div className={styles.gridItem}>
             <p className={styles.title}>Culture Show</p>
@@ -17,10 +20,10 @@ function EventsInfo() {
             <p className={styles.description}>This is an event that features individual and group acts such as singing and dancing, delicious food, and an open dance floor and DJ. In the past, Cal PSA has collaborated with PSAs from other schools in order to host the event and to provide an exciting environment for students to socialize in. Mehndi Ki Raat is open to students and is typically hosted near the end of Fall semester.</p>
         </div>
         <div className={styles.gridItem} style={{textAlign: "right"}}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" alt="" />
+            <img src={MKR} alt="" />
         </div>
         <div className={styles.gridItem}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" alt="" />
+            <img src={Seminar} alt="" />
         </div>
         <div className={styles.gridItem}>
             <p className={styles.title}>Berkeley Aur Baatein</p>
